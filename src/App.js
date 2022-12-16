f939dec7dc35f59ae084f42ca46547fbef9b6b54
YourecMart <Rulet.puzyaka@yandex.ru>
2022-12-16 19:18:55 +0300

    added redux-toolkit/react-persist

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import {
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 import { Router } from "./Router";
 const { Header, Content, Sider } = Layout;
-const items1 = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  }
-);
+
 const App = () => {
   const {
     token: { colorBgContainer },
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
