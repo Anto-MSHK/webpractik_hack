d6b2635bc86d7fc6c220f52c341a80dc8a244b2b
YourecMart <Rulet.puzyaka@yandex.ru>
2022-12-17 02:13:06 +0300

    added folder edit

--- src/components/Folder/Folder.jsx
+++ src/components/Folder/Folder.jsx
@@ -1,18 +1,67 @@
-import { FolderFilled, EditOutlined} from '@ant-design/icons'
-import React from 'react'
+import { FolderFilled, EditOutlined,CheckCircleTwoTone } from '@ant-design/icons'
+import { Input } from 'antd'
+import React, { useState } from 'react'
 import './Folder.css'
 
 export const Folder = () => {
+  const [isEdit, setIsEdit] = useState(false)
+  const [folderName, setFolderName] = useState('')
+  const [date, setDate] = useState('')
+
   return (
     <div className='folder-main'>
-      <div><FolderFilled style={{fontSize: "200px", color: 'gray'}}/></div>
-      <div className='content-folder' style={{display:"flex"}}>
-        <div>Название</div>
-       <div><EditOutlined  style={{fontSize: "20px"}}/></div>
-      </div>
-      <div>
-        Дата
-      </div>
+      {
+
+        isEdit
+          ?
+
+          <div>
+            <div><FolderFilled style={{ fontSize: "200px", color: 'gray' }} /></div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div className='edit-folder'>
+                <Input value={folderName} onChange={(e) => setFolderName(e.target.value)} placeholder="Название" />
+                <Input value={date}  onChange={(e) => setDate(e.target.value)} placeholder='Дата' />
+              </div>
+              <CheckCircleTwoTone   twoToneColor='green' onClick={() => { setIsEdit(!isEdit) 
+                }} style={{ fontSize: "20px" }} />
+            </div>
+
+          </div>
+          :
+          <div>
+
+            <div><FolderFilled style={{ fontSize: "200px", color: 'gray' }} /></div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div className='content-folder'>
+                  <div>
+                    {
+                      folderName 
+                      ?
+                      <span>{folderName}</span>
+                      :
+                      <span>Название</span>
+                    }
+                  
+                    
+                    </div>
+
+                  <div>
+                    {
+                      date 
+                      ?
+                      <span>{date}</span>
+                      :
+                      <span>Дата</span>
+                    }
+                    </div>  
+              </div>
+              <EditOutlined  onClick={() => { setIsEdit(!isEdit) }} style={{ fontSize: "20px" }} />
+            </div>
+
+          </div>
+
+      }
+
     </div>
   )
 }
