4e4cd6a918c68910d570c1fc9fd118058ad7eed0
Rain <silverain1qw@gmail.com>
2022-12-16 22:25:33 +0300

    MainPage fix

--- src/Pages/MainAdminPage/MainAdminPage.jsx
+++ src/Pages/MainAdminPage/MainAdminPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
 import { Avatar, List, Space } from 'antd';
+import './MainAdminPage.css'
+import {Folder} from "../../components/Folder/Folder";
 
 const data = Array.from({
     length: 23,
@@ -22,46 +24,16 @@ const IconText = ({ icon, text }) => (
 
 export const Main = () => {
   return (
-      <List
-          itemLayout="vertical"
-          size="large"
-          style={{height: '100%'}}
-          pagination={{
-              onChange: (page) => {
-                  console.log(page);
-              },
-              pageSize: 3,
-          }}
-          dataSource={data}
-          footer={
-              <div>
-                  <b>ant design</b> footer part
-              </div>
-          }
-          renderItem={(item) => (
-              <List.Item
-                  key={item.title}
-                  actions={[
-                      <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                      <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                      <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-                  ]}
-                  extra={
-                      <img
-                          width={272}
-                          alt="logo"
-                          src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                      />
-                  }
-              >
-                  <List.Item.Meta
-                      avatar={<Avatar src={item.avatar} />}
-                      title={<a href={item.href}>{item.title}</a>}
-                      description={item.description}
-                  />
-                  {item.content}
-              </List.Item>
-          )}
-      />
+    <div className={'mainAdmpage-main'}>
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+        <Folder />
+    </div>
   );
 };
--- src/components/Folder/Folder.jsx
+++ src/components/Folder/Folder.jsx
@@ -1,15 +1,14 @@
-import { FolderTwoTone, EditOutlined} from '@ant-design/icons'
+import { FolderFilled, EditOutlined} from '@ant-design/icons'
 import React from 'react'
-
-import './Folder'
+import './Folder.css'
 
 export const Folder = () => {
   return (
     <div className='folder-main'>
-      <div><FolderTwoTone style={{fontSize: "100px"}}/></div>
+      <div><FolderFilled style={{fontSize: "200px", color: 'gray'}}/></div>
       <div className='content-folder' style={{display:"flex"}}>
         <div>Название</div>
-       <div style={{marginLeft: "10px"}}><EditOutlined  style={{fontSize: "20px"}}/></div>
+       <div><EditOutlined  style={{fontSize: "20px"}}/></div>
       </div>
       <div>
         Дата
